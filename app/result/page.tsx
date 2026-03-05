f955139c15bf348b1b59dc08c1eb5cf872d11fc2
Romulo <zarod2019@gmail.com>
2026-03-05 18:01:11 -0500

    Add confetti when user pass exam

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -2,6 +2,7 @@
 import "./result.css"
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
+import confetti from "canvas-confetti"
 
 export default function ResultPage() {
 
@@ -23,6 +24,54 @@ export default function ResultPage() {
 
   const percentage =
     total > 0 ? Math.round((score / total) * 100) : 0
+
+  // 🎉 trigger confetti if passed
+  if (score >= scoreMin) {
+      setTimeout(() => {
+        fireConfetti()
+      }, 500)
+    }
+
+  function fireConfetti() {
+
+    const duration = 3000
+    const animationEnd = Date.now() + duration
+
+    const defaults = {
+      startVelocity: 30,
+      spread: 360,
+      ticks: 60,
+      zIndex: 1000
+    }
+
+    function randomInRange(min:number, max:number) {
+      return Math.random() * (max - min) + min
+    }
+
+    const interval = setInterval(() => {
+
+      const timeLeft = animationEnd - Date.now()
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval)
+      }
+
+      const particleCount = 50 * (timeLeft / duration)
+
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }
+      })
+
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }
+      })
+
+    }, 250)
+  }
 
   return (
     <>
